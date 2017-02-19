5148cd0fe1b21046fdcafa874643951d9056d7cf
nex3z <litianxing9@gmail.com>
2017-02-19 19:03:08 +0800

    Plot K-Means clustering progress.

--- coursera-machine-learning-python/ex7/plot_progress_k_means.py
+++ coursera-machine-learning-python/ex7/plot_progress_k_means.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 from plot_data_points import plot_data_points
 
 
-def plot_progress_k_means(X, centroids, previous, idx, K, i):
+def plot_progress_k_means(X, history_centroids, idx, K, i):
     plot_data_points(X, idx, K)
-    plt.plot(centroids[:, 0], centroids[:, 1], linestyle='', marker='x', markersize=10, linewidth=3, color='k')
+    plt.plot(history_centroids[0:i+1, :, 0], history_centroids[0:i+1, :, 1],
+             linestyle='', marker='x', markersize=10, linewidth=3, color='k')
     plt.title('Iteration number {}'.format(i + 1))
-    for i in range(centroids.shape[0]):
-        draw_line(centroids[i, :], previous[i, :])
+    print history_centroids.shape
+    for centroid_idx in range(history_centroids.shape[1]):
+        for iter_idx in range(i):
+            draw_line(history_centroids[iter_idx, centroid_idx, :], history_centroids[iter_idx + 1, centroid_idx, :])
 
 
 def draw_line(p1, p2):
--- coursera-machine-learning-python/ex7/run_k_means.py
+++ coursera-machine-learning-python/ex7/run_k_means.py
@@ -9,22 +9,21 @@ from compute_centroids import compute_centroids
 def run_k_means(X, initial_centroids, max_iters, plot_progress=False):
     K = initial_centroids.shape[0]
     centroids = initial_centroids
-    previous_centroids = centroids
+    history_centroids = np.zeros((max_iters, centroids.shape[0], centroids.shape[1]))
+    print history_centroids.shape
     idx = np.zeros(X.shape[0])
-
-    plt.figure()
 
     for i in range(max_iters):
         print 'K-Means iteration {}/{}'.format(i + 1, max_iters)
+        history_centroids[i, :] = centroids
 
         idx = find_closest_centroids(X, centroids)
 
         if plot_progress:
-            plot_progress_k_means(X, centroids, previous_centroids, idx, K, i)
-            previous_centroids = centroids
+            plt.figure()
+            plot_progress_k_means(X, history_centroids, idx, K, i)
+            plt.show()
 
         centroids = compute_centroids(X, idx, K)
-
-    plt.show()
 
     return centroids, idx
